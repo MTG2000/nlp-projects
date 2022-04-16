d1e8525b2ab09a9e3ebf054b933de30ebe2e3676
MTG2000 <mtg0987654321@gmail.com>
2022-04-16 15:50:44 +0300

    fix deployment url

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+console.log(process.env.REACT_APP_GITHUB);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.REACT_APP_GITHUB ? "/nlp-projects/" : undefined}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
